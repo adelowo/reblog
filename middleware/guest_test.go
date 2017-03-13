614943c7135a467ba2fa4233815de514584f54ee
Lanre Adelowo <me@lanreadelowo.com>
2017-03-13 01:32:27 +0100

    Test for guest middleware

---
+++ middleware/guest_test.go
@@ -0,0 +1,55 @@
+package middleware
+
+import (
+	"testing"
+	"net/http"
+	"net/http/httptest"
+	"github.com/adelowo/reblog/handler"
+	"github.com/adelowo/reblog/utils"
+	"github.com/adelowo/reblog/models/mocks"
+	"bytes"
+)
+
+func TestGuest(t *testing.T) {
+	db := &mocks.DataStore{}
+	h := &handler.Handler{DB : db, JWT : utils.NewJWTGenerator()}
+
+	req, err := http.NewRequest("POST", "/login", nil)
+	req.Header.Add("Authorization", "Bearer abc123")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+
+	Guest(http.HandlerFunc(handler.PostLogin(h))).
+		ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusUnauthorized {
+		t.Fatal(status)
+	} else {
+		t.Log("One test passed")
+	}
+
+	req, err = http.NewRequest("POST", "/login", nil)
+
+	rr = httptest.NewRecorder()
+
+	Guest(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("You have been pawned"))
+	})).ServeHTTP(rr, req)
+
+	if status := rr.Code; status != http.StatusOK {
+		t.Fatal(status)
+	} else {
+		t.Log("Yet another test passed")
+	}
+
+	if !bytes.Equal([]byte("You have been pawned"), rr.Body.Bytes()) {
+		t.Fail()
+	} else {
+		t.Log("Yup, another one passed")
+	}
+}
